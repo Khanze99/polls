6d85be626e50c94370901786513b3abe00dbf1b2
Khanze99 <khamidov_anvar@mail.ru>
2020-11-27 01:53:35 +0300

    refactor code and create one model for poll and answers

--- systempolls/polls/services.py
+++ systempolls/polls/services.py
@@ -1,8 +1,7 @@
-from collections import OrderedDict
-
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 
-from .models import Poll, CompletedPoll
+from .models import Poll, CompletedPoll, Choice, Question
 from .serializers import PollSerializer, CompletedPollSerializer
 
 
@@ -21,3 +20,47 @@ def get_result_poll(uid):
         result = PollSerializer(Poll.objects.filter(is_active=True), many=True).data
 
     return result
+
+
+def save_results(data):
+    completed_polls = []
+    questions = data['questions']
+
+    try:
+        poll = Poll.objects.get(id=data['poll_id'])
+    except ObjectDoesNotExist:
+        return False, 'poll_id: {}'.format(data['poll_id'])
+
+    try:
+        user = User.objects.get(id=data['uid'])
+    except ObjectDoesNotExist:
+        user = None
+
+    for question in questions:
+        try:
+            q = Question.objects.get(id=question['question_id'])
+        except ObjectDoesNotExist:
+            return False, 'question_id: {}'.format(question['question_id'])
+
+        type_question = question['type']
+        answer = question['answer']
+
+        if type_question == 'text':
+            completed_polls.append(CompletedPoll(user=user, poll=poll, question=q, text=answer))
+
+        if type_question == 'sc':
+            try:
+                choice = Choice.objects.get(id=answer)
+            except ObjectDoesNotExist:
+                return False, 'choice_id: {}'.format(answer)
+
+            completed_polls.append(CompletedPoll(user=user, poll=poll, question=q, choice=choice))
+
+        if type_question == 'mc':
+            choices = Choice.objects.filter(id__in=answer)
+            for choice in choices:
+                completed_polls.append(CompletedPoll(user=user, poll=poll, question=q, choice=choice))
+
+    CompletedPoll.objects.bulk_create(completed_polls)
+
+    return True, None
--- systempolls/polls/views.py
+++ systempolls/polls/views.py
@@ -1,10 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
-
-from .models import CompletedPoll, Poll, Choice, Question
-from .services import get_result_poll
+from .services import get_result_poll, save_results
 
 
 class PollView(APIView):
@@ -19,46 +15,10 @@ class PollView(APIView):
         return Response(result)
 
     def post(self, request) -> Response:
-        completed_polls = []
         data = request.data
+        flag, error = save_results(data)
 
-        questions = data['questions']
+        if flag is False:
+            return Response({'error': 'bad data {}'.format(error)}, status=400)
 
-        try:
-            poll = Poll.objects.get(id=data['poll_id'])
-        except ObjectDoesNotExist:
-            return Response({'message': 'invalid data'}, status=400)
-
-        try:
-            user = User.objects.get(id=data['uid'])
-        except ObjectDoesNotExist:
-            user = None
-
-        for question in questions:
-            try:
-                q = Question.objects.get(id=question['question_id'])
-            except ObjectDoesNotExist:
-                return Response({'message': 'invalid data'}, status=400)
-
-            type_question = question['type']
-            answer = question['answer']
-
-            if type_question == 'text':
-                completed_polls.append(CompletedPoll(user=user, poll=poll, question=q, text=answer))
-
-            if type_question == 'sc':
-                try:
-                    choice = Choice.objects.get(id=answer)
-                except ObjectDoesNotExist:
-                    return Response({'message': 'invalid data'}, status=400)
-
-                completed_polls.append(CompletedPoll(user=user, poll=poll, question=q, choice=choice))
-
-            if type_question == 'mc':
-                choices = Choice.objects.filter(id__in=answer)
-                for choice in choices:
-                    completed_polls.append(CompletedPoll(user=user, poll=poll, question=q, choice=choice))
-
-        CompletedPoll.objects.bulk_create(completed_polls)
-
-        return Response({"status": 200})
+        return Response({'status': 'created'}, status=201)
