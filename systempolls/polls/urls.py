a27d95a4e65ba2e31732225fffcfb82586fa1ef4
Khanze99 <khamidov_anvar@mail.ru>
2020-11-30 20:35:10 +0300

    rename urls and set name urls

--- systempolls/polls/urls.py
+++ systempolls/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PollView
 
 urlpatterns = [
-    path('active/polls/', PollView.as_view()),
-    path('completed/polls/<int:uid>/', PollView.as_view()),
-    path('take/survey/', PollView.as_view())
+    path('active/polls/', PollView.as_view(), name='get_active_polls'),
+    path('completed/polls/<int:uid>/', PollView.as_view(), name='get_completed_polls'),
+    path('take/poll/', PollView.as_view(), name='post_poll')
 ]
